7e5037f8d7274cab2fb889d81f2595b5bccd505b
Jon Wenger <jonwenger783@gmail.com>
2018-06-20 15:13:30 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -21,4 +21,9 @@ disney = ["lilo", "stich", "walt", "mickey", "miney"]
 books = ["winnie the pooh's magical adventure", "to kill a mocking bird", "educated", "Holt McDougal's principles of algebra 1, "]
 age = [25]
 
-puts books[3]
+books.push("Geometry Textbook")
+puts books
+
+
+
+
