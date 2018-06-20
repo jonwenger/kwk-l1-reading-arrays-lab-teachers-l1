8f1d61fcf6b66cac326d015a7645f4fde674f178
Jon Wenger <jonwenger783@gmail.com>
2018-06-20 14:45:15 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -1,6 +1,7 @@
-student_one = "Caroline"
-student_two = "Millie"
-student_three = "Sam"
-student_four = "Sonali"
+# student_one = "Caroline"
+# student_two = "Millie"
+# student_three = "Sam"
+# student_four = "Sonali"
 
-puts "In this class, we have a student name #{student_four}"
+#puts "In this class, we have a student name #{student_four}"
+
