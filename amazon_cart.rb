6b6a5ba2cc337d74c342cd104e61b04ecdb2333e
Jon Wenger <jonwenger783@gmail.com>
2018-06-20 14:40:04 +0000

    automatically backed up by learn

---
+++ amazon_cart.rb
@@ -0,0 +1,9 @@
+cart_item_prices = [12.43, 19.89, 3.49, 75.00]
+cart_item_prices.push(5.30)
+
+count = 1
+ cart_item_prices.each do |price| # each iterates through everythings in the array
+
+end 
+
+
