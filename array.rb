ec92a6af06504254edcf355fec258cb761b352b7
Jon Wenger <jonwenger783@gmail.com>
2018-06-20 15:06:04 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -18,7 +18,7 @@
 # puts friends
 
 disney = ["lilo", "stich", "walt", "mickey", "miney"]
-books = ["winne the pooh's magical adventure", "to kill a mocking bird", "educated", "Holt McDougal's principles of algebra 1, "]
+books = ["winnie the pooh's magical adventure", "to kill a mocking bird", "educated", "Holt McDougal's principles of algebra 1, "]
 age = [25]
 
 puts books
